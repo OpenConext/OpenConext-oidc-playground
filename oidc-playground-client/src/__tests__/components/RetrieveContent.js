f84759a4f6f23c6bc2cc68151a805ceb232f63ba
Malou <malou_allertz@hotmail.com>
2019-06-04 12:25:11 +0200

    fix retrieve content test

--- oidc-playground-client/src/__tests__/components/RetrieveContent.js
+++ oidc-playground-client/src/__tests__/components/RetrieveContent.js
@@ -7,8 +7,14 @@ it("renders without crashing", () => {
 });
 
 it("disabled button when info is not present", () => {
-  const comp = shallow(<RetrieveContent introspect_endpoint="present" userinfo_endpoint="" />);
+  const comp = shallow(<RetrieveContent introspect_endpoint="present" userinfo_endpoint="" accessToken="abc" />);
 
   expect(comp.find(".introspect").props().disabled).toEqual(false);
   expect(comp.find(".userinfo").props().disabled).toEqual(true);
+});
+
+it("disabled buttons when accessToken is not present", () => {
+  const comp = shallow(<RetrieveContent introspect_endpoint="present" />);
+
+  expect(comp.find(".introspect").props().disabled).toEqual(true);
 });
