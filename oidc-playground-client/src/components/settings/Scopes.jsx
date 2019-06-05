f565e9042bbdbe0aef24ab4d321e3ddc436a7e4a
Malou <malou_allertz@hotmail.com>
2019-06-05 13:40:20 +0200

    remove value.shift which stops any value from being entered

--- oidc-playground-client/src/components/settings/Scopes.jsx
+++ oidc-playground-client/src/components/settings/Scopes.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import {ReactSelect} from "components";
-import {scopesT} from "./Tooltips";
-import {InfoLabel} from "../InfoLabel";
+import { ReactSelect } from "components";
+import { scopesT } from "./Tooltips";
+import { InfoLabel } from "../InfoLabel";
 
 const isOpenIdUsed = props => props.moderators.auth_protocol === "OpenID";
 
-const sanitizeOptions = (isOpenId, options) => isOpenId ? options : options.filter(opt => opt !== "openid");
+const sanitizeOptions = (isOpenId, options) => (isOpenId ? options : options.filter(opt => opt !== "openid"));
 
 export function Scopes(props) {
   let fixedValues = [];
@@ -15,13 +15,11 @@ export function Scopes(props) {
     if (!props.value.includes("openid")) {
       props.value.unshift("openid");
     }
-  } else {
-    props.value.shift();
   }
 
   return (
     <fieldset>
-      <InfoLabel label="Scopes" toolTip={scopesT()}/>
+      <InfoLabel label="Scopes" toolTip={scopesT()} />
       <ReactSelect
         {...props}
         options={sanitizeOptions(isOpenIdUsed(props), props.options)}
