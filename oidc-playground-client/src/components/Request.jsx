fb2f3c76a4afbd51f99d5a9df66e5f6d89523235
Okke Harsta <oharsta@zilverline.com>
2020-02-08 11:28:25 +0100

    Added discovery endpoint

--- oidc-playground-client/src/components/Request.jsx
+++ oidc-playground-client/src/components/Request.jsx
@@ -3,82 +3,85 @@ import JSONPretty from "react-json-pretty";
 import {observer} from "mobx-react";
 import store from "store";
 import {InfoLabel} from "./InfoLabel";
-import {authorizationRequestT, introspectT, tokenRequestT, userInfoT} from "./settings/Tooltips";
+import {authorizationRequestT, introspectT, tokenRequestT, userInfoT, discoveryT} from "./settings/Tooltips";
 
 export const Request = observer(() => {
-  const authorization_url = localStorage.getItem("authorization_url");
+    const authorization_url = localStorage.getItem("authorization_url");
 
-  if (!store.request && !authorization_url) {
+    if (!store.request && !authorization_url) {
+        return (
+            <div className="block no-data">
+                <label>No request data yet.</label>
+            </div>
+        );
+    }
+    const {request_url, request_headers, request_body, result} = store.request || {};
+    const {processingTime} = store;
+
+    const queryParameters = {};
+    if (authorization_url) {
+        const query = authorization_url.substring(authorization_url.indexOf("?") + 1);
+        const urlSearchParams = new URLSearchParams(query);
+        Array.from(urlSearchParams.keys()).forEach(key => queryParameters[key] = urlSearchParams.get(key));
+    }
+    const {requestLabel, toolTip} =
+        (request_url && request_url.endsWith("userinfo")) ? {
+                requestLabel: "UserInfo endpoint",
+                toolTip: userInfoT()
+            } :
+            (request_url && request_url.endsWith("introspect")) ? {
+                    requestLabel: "Introspect endpoint",
+                    toolTip: introspectT()
+                } :
+                (request_url && request_url.endsWith("openid-configuration")) ? {
+                        requestLabel: "Discovery endpoint",
+                        toolTip: discoveryT()
+                    } :
+                    {
+                        requestLabel: "Token Request - Backchannel request",
+                        toolTip: tokenRequestT()
+                    };
+    const tookTime = processingTime ? `- took ${processingTime} ms` : "";
     return (
-      <div className="block no-data">
-        <label>No request data yet.</label>
-      </div>
+        <div className="block">
+            {authorization_url && (
+                <div className="fieldset">
+                    <InfoLabel label="Authorization Request - Browser redirect" toolTip={authorizationRequestT()}
+                               copyToClipBoardText={authorization_url}/>
+                    <input disabled value={authorization_url}/>
+                </div>
+            )}
+            {authorization_url && (
+                <div className="fieldset">
+                    <label>Query parameters</label>
+                    <JSONPretty data={queryParameters}/>
+                </div>
+            )}
+            {request_url && (
+                <div className="fieldset">
+                    <InfoLabel label={requestLabel} toolTip={toolTip} copyToClipBoardText={request_url}/>
+                    <input disabled value={request_url}/>
+                </div>
+            )}
+            {request_headers && (
+                <div className="fieldset">
+                    <label>Headers</label>
+                    <JSONPretty data={request_headers}/>
+                </div>
+            )}
+            {request_body && (
+                <div className="fieldset">
+                    <label>Form parameters</label>
+                    <JSONPretty data={request_body}/>
+                </div>
+            )}
+
+            {result && (
+                <div className="fieldset">
+                    <label>{`Result ${tookTime}`}</label>
+                    <JSONPretty data={result}/>
+                </div>
+            )}
+        </div>
     );
-  }
-
-  const {request_url, request_headers, request_body, result} = store.request || {};
-  const {processingTime} = store;
-
-  const queryParameters = {};
-  if (authorization_url) {
-    const query = authorization_url.substring(authorization_url.indexOf("?") + 1);
-    const urlSearchParams = new URLSearchParams(query);
-    Array.from(urlSearchParams.keys()).forEach(key => queryParameters[key] = urlSearchParams.get(key));
-  }
-  const {requestLabel, toolTip} =
-    (request_url && request_url.endsWith("userinfo")) ? {
-        requestLabel: "UserInfo endpoint",
-        toolTip: userInfoT()
-      } :
-      (request_url && request_url.endsWith("introspect")) ? {
-          requestLabel: "Introspect endpoint",
-          toolTip: introspectT()
-        } :
-        {
-          requestLabel: "Token Request - Backchannel request",
-          toolTip: tokenRequestT()
-        };
-  const tookTime = processingTime ? `- took ${processingTime} ms` : "";
-  return (
-    <div className="block">
-      {authorization_url && (
-        <div className="fieldset">
-          <InfoLabel label="Authorization Request - Browser redirect" toolTip={authorizationRequestT()}
-                     copyToClipBoardText={authorization_url}/>
-          <input disabled value={authorization_url}/>
-        </div>
-      )}
-      {authorization_url && (
-        <div className="fieldset">
-          <label>Query parameters</label>
-          <JSONPretty data={queryParameters}/>
-        </div>
-      )}
-      {request_url && (
-        <div className="fieldset">
-          <InfoLabel label={requestLabel} toolTip={toolTip} copyToClipBoardText={request_url}/>
-          <input disabled value={request_url}/>
-        </div>
-      )}
-      {request_headers && (
-        <div className="fieldset">
-          <label>Headers</label>
-          <JSONPretty data={request_headers}/>
-        </div>
-      )}
-      {request_body && (
-        <div className="fieldset">
-          <label>Form parameters</label>
-          <JSONPretty data={request_body}/>
-        </div>
-      )}
-
-      {result && (
-        <div className="fieldset">
-          <label>{`Result ${tookTime}`}</label>
-          <JSONPretty data={result}/>
-        </div>
-      )}
-    </div>
-  );
 });
--- oidc-playground-client/src/pages/RetrieveContent.jsx
+++ oidc-playground-client/src/pages/RetrieveContent.jsx
@@ -1,76 +1,87 @@
 import React from "react";
 import {observer} from "mobx-react";
 import store from "store";
-import {postIntrospect, postRefreshToken, postUserinfo} from "../api";
+import {discovery, postIntrospect, postRefreshToken, postUserinfo} from "../api";
 
 export const RetrieveContent = observer(props => {
-  const accessToken = store.normalFlowAccessToken || store.hybridFlowAccessToken || store.clientCredentialsAccessToken ||
-    ((store.request || {}).result || {}).access_token;
+    const accessToken = store.normalFlowAccessToken || store.hybridFlowAccessToken || store.clientCredentialsAccessToken ||
+        ((store.request || {}).result || {}).access_token;
 
-  const refreshToken = store.refreshToken || ((store.request || {}).result || {}).refresh_token;
-  const body = {
-    token: accessToken,
-    refresh_token: refreshToken,
-  };
+    const refreshToken = store.refreshToken || ((store.request || {}).result || {}).refresh_token;
+    const body = {
+        token: accessToken,
+        refresh_token: refreshToken,
+    };
 
-  const handleResult = res => {
-    store.request = res;
-    store.activeTab = "Request";
+    const handleResult = res => {
+        store.request = res;
+        store.activeTab = "Request";
 
-    if (res.result && res.result.refresh_token) {
-      store.normalFlowAccessToken = res.result.access_token;
-      store.normalFlowIdToken = res.result.id_token;
-      store.refreshToken = res.result.refresh_token;
-    }
-  };
+        if (res.result && res.result.refresh_token) {
+            store.normalFlowAccessToken = res.result.access_token;
+            store.normalFlowIdToken = res.result.id_token;
+            store.refreshToken = res.result.refresh_token;
+        }
+    };
 
-  const handleError = (err, endpoint) => err.json && err.json().then(
-    res =>
-      (store.message = `Exception returned from endpoint ${endpoint}.
+    const handleError = (err, endpoint) => err.json && err.json().then(
+        res =>
+            (store.message = `Exception returned from endpoint ${endpoint}.
                               Error: ${res.error} (${res.status}). Cause ${res.message}`)
-  );
+    );
 
-  const handleIntrospect = () => postIntrospect(body)
-    .then(handleResult)
-    .catch(err => handleError(err, "introspect"));
+    const handleIntrospect = () => postIntrospect(body)
+        .then(handleResult)
+        .catch(err => handleError(err, "introspect"));
 
-  const handleUserInfo = () => postUserinfo(body)
-    .then(handleResult)
-    .catch(err => handleError(err, "userinfo"));
+    const handleUserInfo = () => postUserinfo(body)
+        .then(handleResult)
+        .catch(err => handleError(err, "userinfo"));
 
-  const handleRefreshToken = () => postRefreshToken({...store.config, ...body})
-    .then(handleResult)
-    .catch(err => handleError(err, "refresh_token"));
+    const handleRefreshToken = () => postRefreshToken({...store.config, ...body})
+        .then(handleResult)
+        .catch(err => handleError(err, "refresh_token"));
 
-  return (
-    <>
-      <div className="tabs">
-        <div className="tab active single-tab">
-          <h2>Retrieve content</h2>
-        </div>
-      </div>
-      <div className="block retrieve-content">
-        <div className="button-group">
-          <button
-            type="button"
-            className="button userinfo"
-            disabled={!(accessToken && !store.clientCredentialsAccessToken)}
-            onClick={handleUserInfo}>Userinfo
-          </button>
-          <button
-            type="button"
-            className="button introspect"
-            disabled={!(accessToken)}
-            onClick={handleIntrospect}>Introspect
-          </button>
-          <button
-            type="button"
-            className="button refresh-token"
-            disabled={!refreshToken}
-            onClick={handleRefreshToken}>Refresh token
-          </button>
-        </div>
-      </div>
-    </>
-  );
+    const handleDiscovery = () => discovery().then(res => {
+        store.request = {result: res, request_url: res.issuer + "/oidc/.well-known/openid-configuration"};
+        store.activeTab = "Request";
+        window.scrollTo(0, 0);
+    });
+
+    return (
+        <>
+            <div className="tabs">
+                <div className="tab active single-tab">
+                    <h2>Retrieve content</h2>
+                </div>
+            </div>
+            <div className="block retrieve-content">
+                <div className="button-group">
+                    <button
+                        type="button"
+                        className="button userinfo"
+                        disabled={!(accessToken && !store.clientCredentialsAccessToken)}
+                        onClick={handleUserInfo}>Userinfo
+                    </button>
+                    <button
+                        type="button"
+                        className="button introspect"
+                        disabled={!(accessToken)}
+                        onClick={handleIntrospect}>Introspect
+                    </button>
+                    <button
+                        type="button"
+                        className="button refresh-token"
+                        disabled={!refreshToken}
+                        onClick={handleRefreshToken}>Refresh token
+                    </button>
+                    <button
+                        type="button"
+                        className="button discovery"
+                        onClick={handleDiscovery}>Discovery
+                    </button>
+                </div>
+            </div>
+        </>
+    );
 });
