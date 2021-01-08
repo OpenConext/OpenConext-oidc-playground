874c32d3f1c97095392aaf94a6aedaee8accf985
Okke Harsta <oharsta@zilverline.com>
2021-01-08 12:15:25 +0100

    Prevent error in devtools console

--- oidc-playground-client/src/components/settings/Scopes.jsx
+++ oidc-playground-client/src/components/settings/Scopes.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { ReactSelect } from "components";
-import { scopesT } from "./Tooltips";
-import { InfoLabel } from "../InfoLabel";
+import React, {useEffect} from "react";
+import {ReactSelect} from "components";
+import {scopesT} from "./Tooltips";
+import {InfoLabel} from "../InfoLabel";
 
 export function Scopes(props) {
-  const { value, options, moderators, onChange } = props;
+  const {value, options, moderators, onChange} = props;
 
   const authIsOpenId = moderators.auth_protocol === "OpenID";
   const hasOpenIdScope = value.includes("openid");
@@ -12,17 +12,19 @@ export function Scopes(props) {
   const fixedValues = authIsOpenId ? ["openid"] : [];
   const sanitizeOptions = () => (authIsOpenId ? options : options.filter(opt => opt !== "openid"));
 
-  if (authIsOpenId && !hasOpenIdScope) {
-    onChange(["openid"].concat(value));
-  }
+  useEffect(() => {
+    if (authIsOpenId && !hasOpenIdScope) {
+      onChange(["openid"].concat(value));
+    }
 
-  if (!authIsOpenId && hasOpenIdScope) {
-    onChange(value.filter(val => val !== "openid"));
-  }
+    if (!authIsOpenId && hasOpenIdScope) {
+      onChange(value.filter(val => val !== "openid"));
+    }
+  });
 
   return (
     <fieldset>
-      <InfoLabel label="Scopes" toolTip={scopesT()} />
+      <InfoLabel label="Scopes" toolTip={scopesT()}/>
       <ReactSelect
         {...props}
         options={sanitizeOptions()}
