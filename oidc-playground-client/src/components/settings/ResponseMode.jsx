5791a8ec4b0cc363440cc8b68fbb4ce3acc3d0f1
Okke Harsta <oharsta@zilverline.com>
2022-12-04 13:16:29 +0100

    Upped node

--- oidc-playground-client/src/components/settings/ResponseMode.jsx
+++ oidc-playground-client/src/components/settings/ResponseMode.jsx
@@ -5,20 +5,20 @@ import {InfoLabel} from "../InfoLabel";
 
 export class ResponseMode extends React.Component {
 
-  sanitizeOptions() {
-    const frontChannelTokenRequest = this.props.moderators.frontChannelTokenRequest;
+    sanitizeOptions() {
+        const {frontChannelTokenRequest} = this.props.moderators;
+        const {options} = this.props;
+        return options
+            .filter(opt => opt !== "refresh_token")
+            .filter(opt => !frontChannelTokenRequest || opt !== "form_post");
+    }
 
-    return this.props.options
-      .filter(opt => opt !== "refresh_token")
-      .filter(opt => !frontChannelTokenRequest || opt !== "form_post");
-  }
-
-  render() {
-    return (
-      <fieldset>
-        <InfoLabel label="Response mode" toolTip={responseModeT()}/>
-        <ReactSelect {...this.props} options={this.sanitizeOptions()}/>
-      </fieldset>
-    );
-  }
+    render() {
+        return (
+            <fieldset>
+                <InfoLabel label="Response mode" toolTip={responseModeT()}/>
+                <ReactSelect {...this.props} options={this.sanitizeOptions()}/>
+            </fieldset>
+        );
+    }
 }
